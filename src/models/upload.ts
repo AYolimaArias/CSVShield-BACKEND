76d71da79866c3249d617c40a858cf1c61525e9b
Angelica <angelica.arias33@gmail.com>
2024-05-15 13:00:56 -0500

    [ADD] Error message and success message in JSON format implemented

--- src/models/upload.ts
+++ src/models/upload.ts
@@ -1,26 +1,31 @@
 import { z } from "zod";
 
 export const userCSVSchema = z.object({
-  name: z.string({
-    required_error: "Name is required",
-    invalid_type_error: "Name have to be a string",
-  }),
+  name: z
+    .string({
+      required_error: "Name is required",
+      invalid_type_error: "Name have to be a string",
+    })
+    .min(1, { message: "Name is required" }),
+
   email: z
     .string({
       required_error: "Email is required",
-      invalid_type_error: "Email must be a string",
+      invalid_type_error: "Email must be a valid format",
     })
     .email({ message: "Email must be valid" }),
-  age: z.number({
-    required_error: "Age is required",
-    invalid_type_error: "Age must be a number",
-  }),
-
-  role: z
-    .enum(["admin", "user"], {
-      errorMap: () => ({ message: "The role must be 'admin' or 'user' " }),
+  age: z
+    .number({
+      required_error: "Age is required",
+      invalid_type_error: "Age must be a positive number",
     })
-    .default("user"),
+    .min(1, { message: "Age must be a positive number" }),
+
+  //   role: z
+  //     .enum(["admin", "user"], {
+  //       errorMap: () => ({ message: "The role must be 'admin' or 'user' " }),
+  //     })
+  //     .default("user"),
 });
 
 export type UserParams = z.infer<typeof userCSVSchema>;
--- src/routers/upload-router.ts
+++ src/routers/upload-router.ts
@@ -3,6 +3,9 @@ import { authenticateHandler } from "../middlewares/authenticate";
 import { authorize } from "../middlewares/authorize";
 import { readFileSync } from "fs";
 import { parse } from "csv-parse/sync";
+import { ApiError } from "../middlewares/error";
+import { UserParams, userCSVSchema } from "../models/upload";
+import { ZodError } from "zod";
 // import { validationHandler } from "../middlewares/validation";
 // import { userCSVSchema } from "../models/upload";
 
@@ -14,20 +17,47 @@ uploadRouter.post(
   authenticateHandler,
   authorize("admin"),
   async (req, res, next) => {
-    const fileCSVContent = readFileSync("./users.csv", "utf-8");
-    const contenCSV = parse(fileCSVContent, {
-      columns: true,
-    });
-    console.log(contenCSV);
     try {
-      const getUsers = await contenCSV;
+      const fileCSVContent = readFileSync("./upload/users.csv", "utf-8");
+      const contentCSV = parse(fileCSVContent, {
+        columns: true,
+        cast: (value, context) => {
+          if (context.column === "age") return Number(value);
+          return value;
+        },
+      });
+      console.log(contentCSV);
+      const successData: UserParams[] = [];
+      const errorData: { row: number; details: Record<string, string> }[] = [];
+
+      // Lógica para procesar los datos y detectar errores
+      for (const [index, record] of contentCSV.entries()) {
+        try {
+          // Validar el registro según el esquema de Zod
+          const validatedRecord = userCSVSchema.parse(record);
+          successData.push(validatedRecord);
+        } catch (validationError: any) {
+          const details: Record<string, string> = {};
+          // Convertir los errores de validación de Zod a un objeto plano de detalles de error
+          (validationError as ZodError).errors.forEach((error) => {
+            details[error.path[0]] = error.message;
+          });
+          // Agregar el registro con errores al array de datos erróneos
+          errorData.push({ row: index + 1, details: details });
+        }
+      }
+      //   const getUsers = await contenCSV;
+      //   if (){}
       res.json({
         ok: true,
         data: {
-          success: [getUsers],
+          success: [successData],
+          error: [errorData],
         },
       });
-    } catch (error) {}
+    } catch (error) {
+      next(new ApiError("Unauthorized", 401));
+    }
   }
 );
 
